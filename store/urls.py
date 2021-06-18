3a540a316f72141f932dd2b3a3efb91deb8ebafc
javad-hub <fasanehsepasian@gmail.com>
2021-06-18 17:28:03 +0430

    wiring up the pages

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import IndexPageView, ShopPageView
+from .views import (IndexPageView, ShopPageView, AboutPageView, CartPageView, CheckOutPageView, ContactPageView, AccountPageView, ServicePageView,
+    ShopDetailPageView, WishListlPageView)
 
 urlpatterns = [
     path('',IndexPageView.as_view(), name="index"),
     path('shop/',ShopPageView.as_view(), name="shop"),
+    path('about/',AboutPageView.as_view(), name="about"),
+    path('cart/',CartPageView.as_view(), name="cart"),
+    path('checkout/',CheckOutPageView.as_view(), name="checkout"),
+    path('contact-us/',ContactPageView.as_view(), name="contact"),
+    path('your-account/',AccountPageView.as_view(), name="my_account"),
+    path('services/',ServicePageView.as_view(), name="service"),
+    path('shop-detail/',ShopDetailPageView.as_view(), name="shop-detail"),
+    path('wishlist/',WishListlPageView.as_view(), name="wishlist"),
 ]
--- store/views.py
+++ store/views.py
@@ -7,3 +7,27 @@ class IndexPageView(TemplateView):
 
 class ShopPageView(TemplateView):
     template_name = 'shop.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+class CartPageView(TemplateView):
+    template_name = 'cart.html'
+
+class CheckOutPageView(TemplateView):
+    template_name = 'checkout.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'contact-us.html'
+
+class AccountPageView(TemplateView):
+    template_name = 'my-account.html'
+
+class ServicePageView(TemplateView):
+    template_name = 'service.html'
+
+class ShopDetailPageView(TemplateView):
+    template_name = 'shop-detail.html'
+
+class WishListlPageView(TemplateView):
+    template_name = 'wishlist.html'
